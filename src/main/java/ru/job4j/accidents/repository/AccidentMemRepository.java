e03bca57f70d6b706a84196f71d1893c36eec7f8
VladislavCat <kshift84@gmail.com>
2023-01-26 10:25:41 +0300

    3. @ModelAttribute. Создание инцидента. [#261013 #384276]

---
+++ src/main/java/ru/job4j/accidents/repository/AccidentMemRepository.java
@@ -0,0 +1,33 @@
+package ru.job4j.accidents.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.accidents.model.Accident;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class AccidentMemRepository implements AccidentRepository {
+    private final ConcurrentMap<Integer, Accident> map = new ConcurrentHashMap<>();
+    private final AtomicInteger indexMap = new AtomicInteger(0);
+
+    public boolean addAccident(Accident accident) {
+        accident.setId(indexMap.getAndIncrement());
+        return map.putIfAbsent(indexMap.get(), accident) == null;
+    }
+
+    public Optional<Accident> findById(int id) {
+        return map.values().stream().filter(accident -> accident.getId() == id).findFirst();
+    }
+
+    public List<Accident> findAll() {
+        return map.values().stream().toList();
+    }
+
+    public void update(Accident accident) {
+        map.replace(accident.getId(), accident);
+    }
+}
--- src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
@@ -1,34 +1,13 @@
 package ru.job4j.accidents.repository;
 
-import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.model.AccidentType;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-@Repository
-public class AccidentRepository {
-    private final ConcurrentMap<Integer, Accident> map = new ConcurrentHashMap<>();
-    private final AtomicInteger indexMap = new AtomicInteger(0);
-
-    public void addAccident(Accident accident) {
-        accident.setId(indexMap.get());
-        map.putIfAbsent(indexMap.getAndIncrement(), accident);
-    }
-
-    public Optional<Accident> findById(int id) {
-        return map.values().stream().filter(accident -> accident.getId() == id).findFirst();
-    }
-
-    public List<Accident> findAll() {
-        return map.values().stream().toList();
-    }
-
-    public void update(Accident accident) {
-        map.replace(accident.getId(), accident);
-    }
+public interface AccidentRepository {
+    boolean addAccident(Accident accident);
+    Optional<Accident> findById(int id);
+    List<Accident> findAll();
+    void update(Accident accident);
 }
