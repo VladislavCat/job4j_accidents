b4f2c8f6fbd71e11865563baa7772c5f13838c7b
VladislavCat <kshift84@gmail.com>
2023-02-06 20:34:16 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/config/SecurityInit.java
@@ -0,0 +1,8 @@
+package ru.job4j.accidents.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+@Configuration
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
